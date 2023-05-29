8b80a91bd55b65d8478c9c406d3e1c0d7497d2b0
Hassaan <hassaan2009@gmail.com>
2023-05-29 12:45:46 +0100

    added comments

--- clean_data.py
+++ clean_data.py
@@ -5,9 +5,9 @@ def clean_df(df):
     '''Removing duplicates, na values, and evening out the number of samples in each category'''
 
     df = functions.drop(df)  # drop duplicates as well
-    df = functions.drop_col_cat(df, 100, 'RiskLevel', 'low risk')
+    df = functions.drop_col_cat(df, 100, 'RiskLevel', 'low risk') #drop 100 low risk samples
     functions.print_df(df)
-    print(functions.count_cat(df, 'RiskLevel'))
+    print(functions.count_cat(df, 'RiskLevel')) # count the number of samples from each class to make sure it has worked
     '''our class distribution for the target variable in now:
        high risk  112        
        low risk   134          
@@ -16,7 +16,9 @@ def clean_df(df):
        after removing 100 low risk samples
     '''
     return df
-df = clean_df(df)
+df = clean_df(df) # call function
+
+# expect error if we save a file with an existing file name
 try:
     functions.write_df(df, r'H:\Datasets', '\clean_MaternalHealthRisk.csv')
 except PermissionError:
